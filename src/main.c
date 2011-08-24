b8f4c9c3414e110ddd77edd3d85e9062b4010bee
Herond Robaina Salles <hrsalles@republica.(none)>
2011-08-23 23:59:14 -0300

    mtxNew implemented.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include "matrix.h"
 
 int main (void) {
+    MTXMatrix mtx_3_3 = mtxNew(3, 3);
+    MTXMatrix mtx_5_6 = mtxNew(5, 6);
+
+    mtxPrint(mtx_3_3);
+    mtxPrint(mtx_5_6);
+
     return 0;
 }
 
--- src/matrix.c
+++ src/matrix.c
@@ -14,13 +14,36 @@
 
 struct MTXMatrix_ST {
     unsigned rows, columns;
-    double **elem_p;
+    double **rep_p;
 };
 
 /* ========================================================================== */
 
 MTXMatrix mtxNew (unsigned rows, unsigned columns) {
-    return NULL;
+    int i, j;
+    MTXMatrix mtx_return = (MTXMatrix) malloc(sizeof(struct MTXMatrix_ST));
+    double* data_p = (double*) malloc(sizeof(double) * rows * columns);
+    double** rep_p = (double**) malloc(sizeof(double*) * rows);
+
+    mtx_return->rows = rows;
+    mtx_return->columns = columns;
+    mtx_return->rep_p = rep_p;
+
+    for (i = 0; i < rows; i ++) rep_p[i] = data_p + i * rows;
+    for (i = 0; i < rows; i ++)
+        for (j = 0; j < columns; j ++)
+            rep_p[i][j] = (i == j) ? 1.0 : 0.0;
+
+    return mtx_return;
+}
+
+void mtxPrint (const MTXMatrix self) {
+    int i, j;
+
+    for (i = 0; i < self->rows; i ++) {
+        for (j = 0; j < self->columns; j ++) printf("%f ", self->rep_p[i][j]);
+        puts("");
+    }
 }
 
 /* ========================================================================== */
